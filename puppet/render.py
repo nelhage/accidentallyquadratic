9d739d6c1d19eb1bc6ad6bbd8a264d9c6ee196ed
Nelson Elhage <nelhage@nelhage.com>
2015-05-04 11:02:33 -0700

    multiple test cases

--- puppet/render.py
+++ puppet/render.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8; -*-
 import matplotlib.pyplot as plt
 import numpy
@@ -12,8 +13,7 @@ with open(sys.argv[1]) as f:
         if not l:
             continue
         bits = l.split()
-        name = bits[0]
-        k = int(re.search('/bench(\d+)\.pp', name).group(1))
+        k = int(bits[0])
         data[k].append(dict(compile=float(bits[1]),
                             apply=float(bits[2]),
                             total=float(bits[3])))
@@ -28,6 +28,7 @@ for v in x:
     err[0].append(mean - min(vals))
     err[1].append(max(vals) - mean)
 
+linear = numpy.poly1d(numpy.polyfit(x, y, 1))
 quadratic = numpy.poly1d(numpy.polyfit(x, y, 2))
 
 xmin = 0
@@ -35,10 +36,12 @@ xmax = int(1.1 * max(x))
 
 plt.errorbar(x, y, yerr=err, marker='.', linestyle='none')
 plt.xlim([xmin, xmax])
+plt.ylim([0, max(y) * 1.3])
 plt.ylabel('runtime')
 plt.xlabel('nodes')
 
 xs = range(xmin, xmax, xmax/100)
 plt.plot(xs, map(quadratic, xs), label='x²')
+plt.plot(xs, map(linear, xs), label='x')
 
 plt.savefig('puppet.png')
