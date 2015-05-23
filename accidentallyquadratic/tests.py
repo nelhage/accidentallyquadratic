c183947ca6f49ec326f18da3327330b79f854632
Nelson Elhage <nelhage@nelhage.com>
2015-05-23 15:04:49 -0700

    refactor

---
+++ accidentallyquadratic/tests.py
@@ -0,0 +1,69 @@
+import atexit
+import shutil
+import subprocess
+import tempfile
+import re
+
+class TestCase(object):
+    @property
+    def name(self):
+        pass
+
+    def generate(self, n):
+        pass
+    def run(self, n):
+        pass
+
+class PuppetTest(object):
+    def __init__(self, name, generate):
+        self.name = 'puppet-' + name
+        self.tmpdir = tempfile.mkdtemp(suffix=name, prefix='aq-puppet-')
+        self.manifest = os.path.join(self.tmpdir, name + ".pp")
+        self._generate = generate
+        atexit.register(lambda: shutil.rmtree(self.tmpdir))
+
+    def generate(self, n):
+        with open(self.manifest, 'w') as fh:
+            fh.write(self._generate(n))
+
+    def run(self, n):
+        env = dict(os.environ)
+        env['BUNDLE_GEMFILE'] = os.path.join(os.environ['HOME'], 'code', 'puppet', 'Gemfile')
+        out = subprocess.check_output(
+            ['bundle', 'exec', 'puppet', 'apply', '--color=false', self.manifest],
+            env=env)
+
+        matches = re.findall(r'^Notice: ([^\n]+) in ([0-9.]+) seconds$',
+                             out, re.S | re.M)
+        times = {}
+        if matches[0][0].startswith('Compiled catalog'):
+            times['compile'] = float(matches[0][1])
+        if matches[1][0].startswith('Applied catalog'):
+            times['apply'] = float(matches[1][1])
+        return times
+
+def generate_depth_n(n):
+    lines = [
+        r'define object() {}',
+        r'node default {'
+        r'object { "%08x": }' % (0,)
+    ]
+    for i in xrange(1, n):
+        lines.append(r'  object { "%08x": require => Object["%08x"] }' % (i, i-1))
+    lines.append(r'}')
+    return "\n".join(lines)
+
+def generate_fanout_n(n):
+    lines = [
+        r'define object() {}',
+        r'node default {'
+    ]
+    for i in xrange(1, n):
+        lines.append(r'  object { "%08x": }' % (i, i-1))
+    lines.append(r'}')
+    return "\n".join(lines)
+
+all_tests = {
+    'puppet.fanout-n': lambda: PuppetTest('fanout-n', generate_fanout_n),
+    'puppet.depth-n': lambda: PuppetTest('depth-n', generate_depth_n),
+}
--- tests.py
+++ tests.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-import tempfile
 import time
-import subprocess
-import re
 import logging
-import shutil
 import optparse
 
-class TestCase(object):
-    @property
-    def name(self):
-        pass
-
-    def generate(self, n):
-        pass
-    def run(self, n):
-        pass
+import accidentallyquadratic
 
 def run_one(tc, n):
     tc.generate(n)
@@ -56,49 +44,6 @@ def exprange(max):
         yield 5*i
         i *= 10
 
-class PuppetTest(object):
-    def __init__(self, name, generate):
-        self.name = 'puppet-' + name
-        self.tmpdir = tempfile.mkdtemp(suffix=name, prefix='aq-puppet-')
-        self.manifest = os.path.join(self.tmpdir, name + ".pp")
-        self._generate = generate
-
-    def generate(self, n):
-        with open(self.manifest, 'w') as fh:
-            fh.write(self._generate(n))
-
-    def run(self, n):
-        env = dict(os.environ)
-        env['BUNDLE_GEMFILE'] = os.path.join(os.environ['HOME'], 'code', 'puppet', 'Gemfile')
-        out = subprocess.check_output(
-            ['bundle', 'exec', 'puppet', 'apply', '--color=false', self.manifest],
-            env=env)
-
-        matches = re.findall(r'^Notice: ([^\n]+) in ([0-9.]+) seconds$',
-                             out, re.S | re.M)
-        times = {}
-        if matches[0][0].startswith('Compiled catalog'):
-            times['compile'] = float(matches[0][1])
-        if matches[1][0].startswith('Applied catalog'):
-            times['apply'] = float(matches[1][1])
-        return times
-
-    def teardown(self):
-        shutil.rmtree(self.tmpdir)
-
-
-def generate_depth_n(n):
-    lines = [
-        r'define object() {}',
-        r'node default {'
-        r'object { "%08x": }' % (0,)
-    ]
-    for i in xrange(1, n):
-        lines.append(r'  object { "%08x": require => Object["%08x"] }' % (i, i-1))
-    lines.append(r'}')
-    return "\n".join(lines)
-
-
 def main(args):
     parser = optparse.OptionParser()
     parser.add_option('-m', '--max',
@@ -114,15 +59,23 @@ def main(args):
 
     options, args = parser.parse_args(args)
 
+    if len(args) != 1 or args[0] not in accidentallyquadratic.all_tests:
+        print "usage: %s [OPTIONS] test" % (sys.argv[0],)
+        print "Tests:"
+        for t in accidentallyquadratic.all_tests:
+            print " - %s" % (t)
+        parser.usage()
+        return 1
+
+    tc = accidentallyquadratic.all_tests[args[1]]()
+
     logging.basicConfig(level=logging.INFO)
-    tc = PuppetTest('depth-n', generate_depth_n)
     try:
         with open(os.path.join("out", tc.name + ".csv"), 'w') as fh:
             results = run_range(tc, exprange(options.max), options.runs)
             render(results, fh)
     finally:
         tc.teardown()
-
 
 if __name__ == '__main__':
     exit(main(sys.argv))
