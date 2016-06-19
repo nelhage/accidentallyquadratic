41766e865dbc3c6c4148ea7022f7b8bb15459b66
Nelson Elhage <nelhage@nelhage.com>
2016-06-19 08:29:06 -0700

    checkpoint sprockets
    ```
                                    user     system      total        real
    old+ascii-short-no          0.000000   0.000000   0.000000 (  0.000917)
    old+ascii-short-yes         0.000000   0.000000   0.000000 (  0.000675)
    old+ascii-long-no           0.000000   0.000000   0.000000 (  0.000730)
    old+ascii-long-yes          0.000000   0.000000   0.000000 (  0.000899)
    old+ascii-long-yes-ws       0.170000   0.000000   0.170000 (  0.170048)
    old+empty                   0.000000   0.000000   0.000000 (  0.000179)
    old+utf8-long-no            0.040000   0.000000   0.040000 (  0.045031)
    old+utf8-long-yes           0.050000   0.000000   0.050000 (  0.045321)
    old+utf8-long-yes-ws       24.850000   0.000000  24.850000 ( 24.846467)
    
    new+ascii-short-no          0.000000   0.000000   0.000000 (  0.000471)
    new+ascii-short-yes         0.000000   0.000000   0.000000 (  0.000390)
    new+ascii-long-no           0.000000   0.000000   0.000000 (  0.000400)
    new+ascii-long-yes          0.000000   0.000000   0.000000 (  0.000448)
    new+ascii-long-yes-ws       0.160000   0.000000   0.160000 (  0.157451)
    new+empty                   0.000000   0.000000   0.000000 (  0.000259)
    new+utf8-long-no            1.040000   0.070000   1.110000 (  1.111428)
    new+utf8-long-yes           1.060000   0.000000   1.060000 (  1.064833)
    new+utf8-long-yes-ws        1.220000   0.010000   1.230000 (  1.223033)
    
    regex+ascii-short-no        0.000000   0.000000   0.000000 (  0.006511)
    regex+ascii-short-yes       0.010000   0.000000   0.010000 (  0.006662)
    regex+ascii-long-no         0.600000   0.000000   0.600000 (  0.600667)
    regex+ascii-long-yes        0.620000   0.000000   0.620000 (  0.620063)
    regex+ascii-long-yes-ws     0.600000   0.000000   0.600000 (  0.611320)
    regex+empty                 0.010000   0.000000   0.010000 (  0.000446)
    regex+utf8-long-no          0.600000   0.000000   0.600000 (  0.599897)
    regex+utf8-long-yes         0.620000   0.000000   0.620000 (  0.622800)
    regex+utf8-long-yes-ws      0.630000   0.000000   0.630000 (  0.632482)
    ```

---
+++ scratch/sprockets.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+require 'benchmark'
+
+def string_end_with_semicolon_new?(str)
+  i = str.size - 1
+  while i >= 0
+    c = str[i].ord
+    i -= 1
+
+    # Need to compare against the ordinals because the string can be UTF_8 or UTF_32LE encoded
+    # 0x0A == "\n"
+    # 0x20 == " "
+    # 0x09 == "\t"
+    # 0x3B == ";"
+    unless c == 0x0A || c == 0x20 || c == 0x09
+      return c === 0x3B
+    end
+  end
+
+  true
+end
+
+def string_end_with_semicolon_orig?(str)
+  i = str.size - 1
+  while i >= 0
+    c = str[i]
+    i -= 1
+    if c == "\n" || c == " " || c == "\t"
+      next
+    elsif c != ";"
+      return false
+    else
+      return true
+    end
+  end
+  true
+end
+
+def string_end_with_semicolon_regex?(str)
+  str =~ /;[ \n\t]*\z/
+end
+
+LONGKB = 100
+N = 1000
+
+def main
+  tests = [
+    {name: "ascii-short-no", value: "x"*1024},
+    {name: "ascii-short-yes", value: "x"*1024 + ";"},
+    {name: "ascii-long-no", value: "x"*1024*LONGKB},
+    {name: "ascii-long-yes", value: "x"*1024*LONGKB + ";"},
+    {name: "ascii-long-yes-ws", value: "x"*1024*LONGKB + ";" + " "*1024},
+    {name: "empty", value: ""},
+    {name: "utf8-long-no", value: "\u2665" + "x"*1024*LONGKB},
+    {name: "utf8-long-yes", value: "\u2665" + "x"*1024*LONGKB + ";"},
+    {name: "utf8-long-yes-ws", value: "\u2665" + "x"*1024*LONGKB + ";" + " "*1024},
+  ]
+  methods = [
+    {name: "old", method: ->x{string_end_with_semicolon_orig?(x)}},
+    {name: "new", method: ->x do
+       x = x.encode(Encoding::UTF_32LE) unless x.ascii_only?
+       string_end_with_semicolon_new?(x)
+     end},
+    {name: "regex", method: ->x{string_end_with_semicolon_regex?(x)}},
+  ]
+
+  Benchmark.benchmark(Benchmark::CAPTION, 25) do |bm|
+    methods.each do |m|
+      puts "# #{m[:name]}"
+      tests.each do |t|
+        bm.report("#{m[:name]}+#{t[:name]}") do
+          for _ in 1..N
+            m[:method].call(t[:value])
+          end
+        end
+      end
+      puts
+    end
+  end
+end
+
+if __FILE__ == $0
+  main
+end
